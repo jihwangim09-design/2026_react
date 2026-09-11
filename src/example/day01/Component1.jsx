8139f2f0b408719034320bd49d35b855e20a317f
jihwangim09-design <jihwangim09@gmail.com>
2026-09-11 17:40:02 +0900

    09 11 1739

--- src/example/day01/Component1.jsx
+++ src/example/day01/Component1.jsx
@@ -1,12 +1,16 @@
 /*
     1. 컴포넌트명.jsx 파일 생성한다.
     2. export default function 컴포넌트명( props ){ }
-       - export defult : 내보내기 , 다른 파일에서 import 할 수 있도록
-            - 만약에 내부에서 사용하는 컴포넌트 생략
+       - export defult : 내보내기 , 다른 파일에서 import 할 수 있도록(가져다 쓸 수 있도록)
+            - 만약에 내부에서 사용하는 컴포넌트는 생략
+            - 파일당 export default는 딱 하나만 가능
+    ex) import Header from './Header';   // Header.jsx에서 내보낸 걸 가져옴
 
        컴포넌트명 : 첫글자 무조건 대문자로 시작
-    3. { } 안에 return 에서는 JSX문법가능 , 그외 JS 문법
+    3. { } 안에 return 에서는 JSX문법가능 , 그외 JS 문법 return 뒤에는 최상위 태그가 반드시 딱 1개여야 함
        return 에서는 2줄이상 입력시 ( < > < > ) 묶는다.
+       <> </> : Fragment (프래그먼트)
+       <> </>는 "여러 개를 묶어주는 역할만 하고, 실제 HTML에는 흔적을 남기지 않는 역할 최상위 태그 1개 만족
 
  
 */
@@ -21,9 +25,12 @@ function Header( props ){        // 헤더
 
 }
 export default function Component1( props ){    // 본문
+    // Component1은 이 페이지는 헤더 + 본문내용 + 푸터로 구성된다"라고 조립 설계도를 그린 것
+    // <Header> </Header>는 HTML 태그처럼 생겼지만 사실은 함수 호출
+    // < /> → JSX 전용 문법, "컴포넌트를 렌더링 하라는 지시" ()를 안씀
     return (<>
 
-    <Header> </Header>
+    <Header> </Header> 
 
     <div> 메인페이지 </div>
 
@@ -32,3 +39,6 @@ export default function Component1( props ){    // 본문
 
     </>)
 }
+
+// Header, Footer는 "레고 블록"이고, Component1은 그 블록들을 조립해서 만든 완성품
+// 사용이유 : 공통되는 UI를 한 곳에서만 관리해서 재사용성과 유지보수성을 높이기 위해서
--- src/example/day01/Component2.jsx
+++ src/example/day01/Component2.jsx
@@ -7,13 +7,16 @@ function FrontComp( props ) {
             <ul>
                 <li>HTML5</li>
                 <li>CSS3</li>
-                <li>Javascirpt</li>
+                <li>Javascript</li>
                 <li>jQuery</li>
             </ul>
     </>)
 }
 // 2. // 화살표 함수 형식으로 작성한 컴포넌트
-const BackComp = ( props ) => { 
+const BackComp = ( props ) => {  // const 변수명 = (매개변수) => { }
+    // props) => { return (...) } 이 통째로가 함수 하나의 값 이거를 BackComp라는 상수에 넣음 
+    // 이후에 BackComp(...) 쓰면 BackComp에 저장된 함수가 실행됨
+    // 이름없는 함수를 만들어서 변수에 붙힌거일뿐 일반 함수 형식이랑 똑같음
     return (<>
         <li>백엔드</li>
         <ul>
@@ -25,7 +28,8 @@ const BackComp = ( props ) => {
     </>)
 }
 // 3. 익명 함수 형식으로 작성한 컴포넌트 생성
-let FormComp = function ( props ) { 
+let FormComp = function ( props ) { // 보통 function FormComp(props){} 이렇게 이름이 바로 붙는데 얘는 안붙었음
+    // onMyEvent1={() => { alert(...) }}처럼 버튼 클릭 시 딱 한 번 실행되고 끝날 동작(재사용안함)을 만들 때 익명함수를 씀
     return (<>
         <form>
             <select name="gubun">
@@ -39,6 +43,7 @@ let FormComp = function ( props ) {
 }
 // 4. 3가지의 컴포넌트 합치는 컴포넌트 , 79p와 다르게 App -> Component2
 export default function Component2 ( props ) { // 최상위 컴포넌트
+    // < /> → JSX 전용 문법, "컴포넌트를 렌더링 하라는 지시" ()를 안씀
     return (<>
     <div>
         <h2>React - Component</h2>
