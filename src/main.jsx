bd3b561a4930b955e8c155d0fc41e6065cd2ef53
jihwangim09-design <jihwangim09@gmail.com>
2026-09-15 09:45:30 +0900

    0915 0945

--- src/main.jsx
+++ src/main.jsx
@@ -26,5 +26,5 @@ const create = createRoot( root );
 // import Component1 from "./example/day02/Component1";
 // create.render( <Component1> </Component1>)
 
-import Component4 from "./example/day02/Component4";
-create.render( <Component4 /> )
+import Productmanager from "./example/day02/totalpractice/ProductManager.jsx"
+create.render( <Productmanager /> )
