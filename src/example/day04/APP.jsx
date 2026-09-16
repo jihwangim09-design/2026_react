44daa971f2e015a28e92236b34d7648837bbc36b
jihwangim09-design <jihwangim09@gmail.com>
2026-09-16 10:38:12 +0900

    0916 1038

--- src/example/day04/APP.jsx
+++ src/example/day04/APP.jsx
@@ -1,3 +1,10 @@
-export default function App(props){
-    return(<> 라우터 실습 </>)
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+export default function App( props ){
+    return (<> 
+        <Routes> {/* 여기에 들어가는 경로들은 주소정의에 따라 렌더링 */}
+            <Route path="/home" element={ <Home/> } />
+        </Routes>
+    </>)
 }
+// <Route path="/도메인이후주소정의" element={ <컴포넌트/> } />
