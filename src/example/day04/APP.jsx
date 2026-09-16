7acec6f8b532b54157701ccd08977c33861457ca
jihwangim09-design <jihwangim09@gmail.com>
2026-09-16 10:59:13 +0900

    0916 1059

--- src/example/day04/APP.jsx
+++ src/example/day04/APP.jsx
@@ -1,9 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
+import TopNavi from "./TopNavi";
+import NotFound from "./NotFound";
 export default function App( props ){
     return (<> 
+        <TopNavi></TopNavi>
         <Routes> {/* 여기에 들어가는 경로들은 주소정의에 따라 렌더링 */}
-            <Route path="/home" element={ <Home/> } />
+            <Route path="/" element={ <Home/> } />
+            <Route path="*" element={ <NotFound/> } />
         </Routes>
     </>)
 }
