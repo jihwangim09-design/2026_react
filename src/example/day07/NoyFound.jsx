8f6bc0561451bf78f3cdce48d72e3a90ca5e41e1
jihwangim09-design <jihwangim09@gmail.com>
2026-09-23 11:38:03 +0900

    0923 1137

--- src/example/day07/NoyFound.jsx
+++ src/example/day07/NoyFound.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function NotFound(props){
     // html에서는 <br> 이렇게 해도 되는데 react에서는 <br/> 닫아야 작동
+    // 라우터 : 특정 url 경로로 페이지로 이동
     return(<>
     <h2>Not Found</h2>
     <P>
